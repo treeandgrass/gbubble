1903f845d55af3058f4aa7b63682a5baefc458a2
treeandgrass <mymymymypcy@163.com>
2020-01-23 16:10:39 +0800

    feat: Encapsulation renderer

--- examples/triangle/simple.ts
+++ examples/triangle/simple.ts
@@ -1,23 +1,13 @@
 import { vec3 } from "gl-matrix";
 import {
-    ContainerShader, createWebGLRenderingContext, glClear, GNode, PerspectiveCamera, Program, simpeFsSOurce,
-    simpleVsSource, WebGLRenderer,
+    glClear, GNode, PerspectiveCamera,
+    SimpleGLRenderer,
 } from "../../src";
 
-const baseRule: string = "#include<([A-z,0-9,.]+)>;";
-const gl: WebGLRenderingContext = createWebGLRenderingContext("canvas");
-const program: Program = new Program(gl);
-
-const vsShader: ContainerShader = new ContainerShader(program, simpleVsSource, baseRule);
-vsShader.setMainShader();
-const fsShader: ContainerShader = new ContainerShader(program, simpeFsSOurce, baseRule);
-fsShader.setMainShader("f");
-
-const webglProgram: WebGLProgram = program.build();
-
+const selector: string = "#canvas";
 // camera
 const fieldOfView: number = 45 * Math.PI / 180;
-const canvas: HTMLCanvasElement = gl.canvas as HTMLCanvasElement;
+const canvas: HTMLCanvasElement = document.querySelector(selector) as HTMLCanvasElement;
 const aspect = canvas.clientWidth / canvas.clientHeight;
 const zNear: number = 0.1;
 const zFar: number = 1000.0;
@@ -30,15 +20,19 @@ const target: vec3 = vec3.fromValues(0, 0, 0.1);
 const up: vec3 = vec3.fromValues(2, 3, 4);
 camera.lookAt(eye, target, up);
 
-const node: GNode = new GNode();
-const renderer = new WebGLRenderer(camera, node, webglProgram, gl);
+
+
+const gNode: GNode = new GNode();
+const renderer = new SimpleGLRenderer(camera, gNode);
+renderer.bind(selector);
+
+
 const vertex1: number[] = [
     -10.0,  10.0,
     10.0,  10.0,
     -10.0, -10.0,
     10.0, -10.0,
 ];
-
 const vertex2: number[] = [
     -17.0,  10.0,
     30.0,  5.0,
@@ -48,12 +42,11 @@ const vertex2: number[] = [
 
 const position1 = new Float32Array(vertex1);
 const position2 = new Float32Array(vertex2);
-
 const positionBuffer1: WebGLBuffer = renderer.createBuffer(position1);
 const positionBuffer2: WebGLBuffer = renderer.createBuffer(position2);
 
 function animation() {
-    glClear(gl);
+    renderer.clear();
     renderer.attachPositionBuffer(positionBuffer1);
     renderer.run();
     renderer.attachPositionBuffer(positionBuffer2);
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export const NODE_TYPE = "NODE";
 export const PERSPECTIVECAMERA_TYPE = "PERSPECTIVE";
 export const ORTHOGRAPHICAMERA_TYPE = "ORTHOGRAPHICAMERA";
+// shader regexp
+export const SHADER_RE: string = "#include<([A-z,0-9,.]+)>;";
