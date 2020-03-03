6033f1b831dbc65eb0b0cc68cff5b85ea8eba855
treeandgrass <mymymymypcy@163.com>
2020-03-03 21:46:07 +0800

    feat: world matrix

--- src/node/GNode.ts
+++ src/node/GNode.ts
@@ -1,14 +1,15 @@
 import { mat4, vec3 } from "gl-matrix";
-import { Renderer } from "../backend";
 import { Binding } from "../backend/binding";
 import { NODE_TYPE } from "../constants";
 import { Event } from "../events";
 import { uuid } from "../utils";
+import { Position } from "./position";
 
 export class GNode extends Event {
     public uuid: string;
     public type: string;
     public children: GNode[];
+    public position: Position;
 
     constructor() {
         super();
@@ -16,6 +17,7 @@ export class GNode extends Event {
         this.type = NODE_TYPE;
         this.uuid = uuid();
         this.children = [];
+        this.position = new Position();
     }
 
     public addChild(node: GNode)  {
@@ -28,5 +30,26 @@ export class GNode extends Event {
 
     public render(gl: WebGLRenderingContext | WebGL2RenderingContext, binding: Binding) {
         // extend by child
+    }
+
+    // set position
+    public setPosition(x: number, y: number, z: number) {
+        this.position.x = x;
+        this.position.y = y;
+        this.position.z = z;
+    }
+
+    private lookAt(): mat4 {
+        const out: mat4 = mat4.create();
+        const eye: vec3 = this.position.toVector();
+        const target: vec3 = vec3.fromValues(0, 0, 0);
+        const up: vec3 = vec3.fromValues(0, 1, 0);
+        mat4.lookAt(out, eye, target, up);
+        mat4.invert(out, out);
+        return out;
+    }
+
+    public get worldMatrix() {
+        return this.lookAt();
     }
 }
---
+++ src/node/position.ts
@@ -0,0 +1,17 @@
+import { vec3 } from "gl-matrix";
+
+export class Position {
+    public x: number;
+    public y: number;
+    public z: number;
+
+    constructor(x: number = 0, y: number = 0, z: number = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public toVector() {
+        return vec3.fromValues(this.x, this.y, this.z);
+    }
+}
